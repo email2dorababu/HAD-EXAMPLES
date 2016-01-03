144b9f198d806d980236b3a8159fed1d20648533
ubuntu <ubuntu@ubuntu-VirtualBox>
2016-01-03 14:35:30 -0500

    Commit

--- HdfsJavaApi/src/org/bigdatatech/HdfsToLocalClient.java
+++ HdfsJavaApi/src/org/bigdatatech/HdfsToLocalClient.java
@@ -5,13 +5,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.util.Progressable;
 
-public class LocalToHdfsJavaClient {
+public class HdfsToLocalClient {
 
 	// java client to copy file from Local file system to HDFS
 	
@@ -20,15 +21,15 @@ public class LocalToHdfsJavaClient {
 		
 		//args[0] = "/home/ubuntu/workspace/InputData/ExampleFile.txt" ;
 		//args[1] = "/user/LocalToHdfs/" ;
-		
+		/* 
 		
 		if(args.length<2){
-			System.err.println("LocalToHdfsJavaClient [Local input path] [HDFS output path] ");
+			System.err.println("LocalToHdfsJavaClient [HDFS output path] [Local input path] ");
 			System.exit(-1);
 			
-		}
-		Path hdfsOutputPath=new Path(args[1]);
-		String localInputPath=args[0]; // local path is just a string
+		} */
+		Path hdfsInputPath=new Path(args[0]);
+		//String localInputPath=args[1]; // local path is just a string
 		Configuration conf=new Configuration();
 		//conf.set("fs.default.name", "hdfs://localhost:50070");
 		conf.addResource(new Path("/home/ubuntu/YARNBOX/hadoop-2.7.1/etc/hadoop/core-site.xml"));
@@ -36,10 +37,12 @@ public class LocalToHdfsJavaClient {
 		//FileSystem is from org.apache.hadoo.fs package
 		FileSystem fs=FileSystem.get(conf);
 		
+		/* 
 		//Once you have the FileSystem instance, Open Output Stream 
 		FSDataOutputStream os=fs.create(hdfsOutputPath);
-		
-		//with progressible
+		*/
+		// instead of create on file system, now use open method
+		FSDataInputStream is=fs.open(hdfsInputPath);
 		/* 
 		FSDataOutputStream os=fs.create(hdfsOutputPath, new Progressable(){
 			public void progress(){
@@ -48,9 +51,11 @@ public class LocalToHdfsJavaClient {
 		}); 
 		*/
 		
-		InputStream is=new BufferedInputStream(new FileInputStream(localInputPath));
+		//InputStream is=new BufferedInputStream(new FileInputStream(localInputPath));
 		// The above InputStream,BufferedInputStream and FileInputStream are from java.io
-		IOUtils.copyBytes(is, os,4096,true);
+		IOUtils.copyBytes(is, System.out,4096,false); // if you mention true here, the stream is autoclosed.
+		is.seek(0);
+		IOUtils.copyBytes(is, System.out,4096,true);
 		System.out.println("FileTransfer completed");
 		
 
